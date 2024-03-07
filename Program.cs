c3c1c3ef76b79bc33a284b8cabf2278bea13b85b
callaito <radelinrsm029@gmail.com>
2024-03-07 15:23:42 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string[] pelicula = new string[3];
+string[] categoria = new string[3];
+
+for (int i = 0; i < pelicula.Length; i++)
+{
+    Console.WriteLine("Introduzca las peliculas: {0}"+(i+1));
+    Console.WriteLine();
+    pelicula[i] = Console.ReadLine();
+
+    Console.WriteLine("Introduzca las categoria: {0}"+(i+1));
+    categoria[i] = Console.ReadLine();
+
+
+    
+}
+
+for (int i = 0; i < pelicula.Length; i++)
+{ Console.WriteLine("Pelicula: {0} , Categoria  {1}" , pelicula[i], categoria[i]);
+}
